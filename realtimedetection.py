f20d55b7b98951e151c62b8d4c8a6ad06a2e61e6
Anagha <119687842+anaghaM10@users.noreply.github.com>
2024-04-25 23:25:07 +0530

    emotion detection code

--- realtimedetection.py
+++ realtimedetection.py
@@ -1,6 +1,11 @@
 import cv2
 from keras.models import model_from_json
 import numpy as np
+import libcamera
+from picamera2 import Picamera2
+import RPi.GPIO as GPIO
+import time
+#import Picamera2
 # from keras_preprocessing.image import load_img
 json_file = open("emotiondetector.json", "r")
 model_json = json_file.read()
@@ -10,16 +15,60 @@ model = model_from_json(model_json)
 model.load_weights("emotiondetector.h5")
 haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade=cv2.CascadeClassifier(haar_file)
+cv2.startWindowThread()
+
+red_pin=26
+green_pin=27
+blue_pin=17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red_pin,GPIO.OUT, initial = 1)
+GPIO.setup(green_pin,GPIO.OUT, initial = 1)
+GPIO.setup(blue_pin,GPIO.OUT, initial = 1)
 
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
+    
+def red():
+ GPIO.output(red_pin,GPIO.LOW)
+ GPIO.output(green_pin,GPIO.HIGH)
+ GPIO.output(blue_pin,GPIO.HIGH)
+ 
+def turnoff():
+    GPIO.output(red_pin,GPIO.HIGH)
+    GPIO.output(green_pin,GPIO.HIGH)
+    GPIO.output(blue_pin,GPIO.HIGH)
+ 
 
-webcam=cv2.VideoCapture(0)
+def yellow():
+ GPIO.output(red_pin,GPIO.LOW)
+ GPIO.output(green_pin,GPIO.LOW)
+ GPIO.output(blue_pin,GPIO.HIGH)
+ 
+def white():
+    GPIO.output(red_pin,GPIO.LOW)
+    GPIO.output(green_pin,GPIO.LOW)
+    GPIO.output(blue_pin,GPIO.LOW)
+    
+def blue():
+    GPIO.output(red_pin,GPIO.HIGH)
+    GPIO.output(green_pin,GPIO.HIGH)
+    GPIO.output(blue_pin,GPIO.LOW) 
+
+camera = "/base/soc/i2c0mux/i2c@1/ov5647@36"
+pipeline = "libcamerasrc camera-name=%s ! video/x-raw,width=640,height=480,framerate=10/1,format=RGBx ! videoconvert ! videoscale ! video/x-raw,width=640,height=480,format=BGR ! appsink" % (camera)
+#webcam=cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888' , "size": (640,480)}))
+picam2.start()
 labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 while True:
-    i,im=webcam.read()
+    #if not webcam.isOpened():
+    #    print("Camera not opened")
+    #i,im=webcam.read()
+    im = picam2.capture_array()
     gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     faces=face_cascade.detectMultiScale(im,1.3,5)
     try: 
@@ -34,7 +83,26 @@ while True:
             f = open("emotiondata.txt", "w")
             f.write(prediction_label)
             #do if else for prediction label to change led light
-            #if prediction_label == 'sad':
+            if prediction_label == 'happy':
+                turnoff()
+                yellow()
+                time.sleep(1)
+            elif prediction_label == 'sad' or prediction_label == 'fear':
+                turnoff()
+                blue()
+                time.sleep(1)
+            elif prediction_label == 'angry':
+                turnoff()
+                red()
+                time.sleep(1)
+            elif prediction_label == 'neutral':
+                turnoff()
+                white()
+                time.sleep(1)
+            else:
+                turnoff()
+                time.sleep(1)
+
             #   write code to light led to desired color
             #elif prediction_label == 'neutral':
             #   write code to light led to desired color
@@ -44,3 +112,5 @@ while True:
         cv2.waitKey(27)
     except cv2.error:
         pass
+    except KeyboardInterrupt:
+                GPIO.cleanup()
